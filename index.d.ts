1988663733aa9b0ed5abe5d5534a44cdd481d1b1
jaywcjlove <398188662@qq.com>
2020-06-18 23:57:49 +0800

    chore: Update index.d.ts #3

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,7 @@
-export default function _default(): any;
+interface Options {
+  remove?: 'effects';
+  removeAll?: boolean;
+  test?: string | RegExp;
+}
+
+export default function _default(code: string, options: Options): any;
