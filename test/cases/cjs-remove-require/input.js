a95370440e8c597c1f5bef075506662b1eca8097
Slava Fomin II <slava@fomin.io>
2020-12-25 13:52:58 +0300

    Fixed logic to remove only `require()` calls and leave other function calls intact
    Signed-off-by: Slava Fomin II <slava@fomin.io>

--- test/cases/cjs-remove-require/input.js
+++ test/cases/cjs-remove-require/input.js
@@ -5,6 +5,9 @@ const b = 2;
 // Variable declarations (SHOULD be removed)
 const core = require('@babel/core');
 
+// Using non-require function
+const something = useCore('@babel/core');
+
 // Using as argument to a function (SHOULD be removed)
 useCore(require('@babel/core'));
 
@@ -17,6 +20,6 @@ require('jest');
 console.log(a + b); // 3
 
 
-function useCore() {
+function useCore(arg0) {
   // do something with it
 }
